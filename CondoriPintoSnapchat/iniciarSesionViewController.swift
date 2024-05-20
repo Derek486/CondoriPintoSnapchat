f64c3b8262511db0e60b72699206ce603625c3b3
JJCondoriPinto <juanjosecondoripinto@gmail.com>
2024-05-20 14:57:31 -0500

    autenticación con google

--- CondoriPintoSnapchat/iniciarSesionViewController.swift
+++ CondoriPintoSnapchat/iniciarSesionViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 import Firebase
 import FirebaseAuth
+import GoogleSignIn
 
 class iniciarSesionViewController: UIViewController {
 
@@ -19,6 +20,48 @@ class iniciarSesionViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func iniciargoogle(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+          guard error == nil else {
+            return
+          }
+
+          guard let user = result?.user,
+            let idToken = user.idToken?.tokenString
+          else {
+            return
+          }
+
+          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                         accessToken: user.accessToken.tokenString)
+            
+            Auth.auth().signIn(with: credential) { result, error in
+
+                if error != nil {
+                    print("Error \(error!)")
+                } else {
+                    
+                    print("Inicio de sesion correcto con GOOGLE")
+                }
+            }
+                
+          // ...
+        }
+    }
+    
+    @IBAction func tappedSigniN(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var signInButton: GIDSignInButton!
     
     @IBAction func iniciarSesionTapped(_ sender: Any) {
         
